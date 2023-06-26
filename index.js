58e55f4fe7d3a2a8e98ef5d4784d360f1b147cc7
anushahchinta345 <79196086+anushahchinta345@users.noreply.github.com>
2023-06-26 16:12:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("amazon");
+  } else if (i % 3 === 0) {
+    console.log("google");
+  } else if (i % 5 === 0) {
+    console.log("facebook");
+  } else {
+    console.log(i);
+  }
+}
